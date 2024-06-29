dcb444429fb5a9fc0be7ab2c7871342d2e96ade8
Hossam Shawky <hossamshawky288@gmail.com>
2024-06-29 11:16:08 +0300

    add new public subnet

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-# output "eks-cluster-name" {
-#   value = module.eks-cluster.eks-cluster-name
-# }
+output "eks-cluster-name" {
+  value = module.EKS.eks_arn
+}
+
+output "public_subnets" {
+  value = module.Public-Subnet.public_subnets_ids
+}
