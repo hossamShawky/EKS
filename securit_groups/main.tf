e9eb59059e52311dc1ba122192b36b2ad4da6faa
Hossam Shawky <hossamshawky288@gmail.com>
2024-06-30 00:54:52 +0300

    node SG
    Allow SSH Connection To Nodes

--- securit_groups/main.tf
+++ securit_groups/main.tf
@@ -50,6 +50,14 @@ resource "aws_security_group" "eks-cluster-SG" {
 
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
